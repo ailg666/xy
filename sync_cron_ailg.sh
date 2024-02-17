8deadc86ea8744acc9c06b61efb473829285b217
i_xxg <29580170@qq.com>
2024-02-17 03:42:11 +0000

    update sync_cron_ailg.sh.
    Signed-off-by: i_xxg <29580170@qq.com>

--- sync_cron_ailg.sh
+++ sync_cron_ailg.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 time_value=${3//：/:}
 hour=${time_value%%:*}
 minu=${time_value#*:}
@@ -6,10 +7,10 @@ if ! [[ "$hour" =~ ^([01]?[0-9]|2[0-3])$ ]] || ! [[ "$minu" =~ ^([0-5]?[0-9])$ ]
   echo "输入错误，请重新输入。小时必须为0-23的正整数，分钟必须为0-59的正整数。"
 fi
 
-echo -e "hour=$hour"
-echo -e "minu=$minu"
-echo -e "$1\n$2\n$3\n$4\n$5\n$6\n$7"
-read -ep "**check"
+#echo -e "hour=$hour"
+#echo -e "minu=$minu"
+#echo -e "$1\n$2\n$3\n$4\n$5\n$6\n$7"
+#read -ep "**check"
 if command -v crontab >/dev/null 2>&1; then
 	crontab -l |grep -v sync_emby_config > /tmp/cronjob.tmp
 	echo '$minu $hour */$4 * * bash -c "$(curl https://xy.ggbond.org/xy/sync_emby_config_ailg.sh)" -s' " $1 $2 $5 $6 >> $1/resilio/cron.log" >> /tmp/cronjob.tmp
