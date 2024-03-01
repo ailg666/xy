7f0b1057650ffece49dda11e7337053704a4b894
i_xxg <29580170@qq.com>
2024-03-01 12:12:40 +0000

    update hlw1.sh.
    Signed-off-by: i_xxg <29580170@qq.com>

--- hlw1.sh
+++ hlw1.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 #!/bin/bash
-docker run -d --name emby2 -v /etc/nsswitch.conf:/etc/nsswitch.conf -v /mnt/media_rw/d48ded09-158b-4536-b78b-0279c6936327/.ugreen_nas/312373/Docker/xiaoyadata/config:/config -v /mnt/media_rw/d48ded09-158b-4536-b78b-0279c6936327/.ugreen_nas/312373/Docker/xiaoyadata/xiaoya:/media --user 0:0 --net=host --add-host="xiaoya.host:192.168.0.105" --restart always emby/embyserver:4.8.0.56
+docker run -d --name emby2 -v /etc/nsswitch.conf:/etc/nsswitch.conf -v /mnt/media_rw/d48ded09-158b-4536-b78b-0279c6936327/.ugreen_nas/312373/Docker/xiaoyadata/config:/config -v /mnt/media_rw/d48ded09-158b-4536-b78b-0279c6936327/.ugreen_nas/312373/Docker/xiaoyadata/xiaoya:/media -v /mnt/dm-0/.ugreen_nas/312373/影视:/movie1 -v /mnt/dm-2/.ugreen_nas/312373/影视:/movie2 --user 0:0 --net=host --add-host="xiaoya.host:192.168.0.105" --restart always emby/embyserver:4.8.0.56
