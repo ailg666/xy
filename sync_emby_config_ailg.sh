5968b4a48f867c627fff5e78d26f5b5ac2eeff86
ailg <ailg666@hotmail.com>
2024-03-06 10:07:49 +0800

    complete test

--- sync_emby_config_ailg.sh
+++ sync_emby_config_ailg.sh
@@ -28,12 +28,6 @@ else
 	EMBY_NAME=emby
 fi
 
-#if [ $4 ]; then
-#	RESILIO_NAME=$4
-#else	
-#	RESILIO_NAME=resilio
-#fi
-
 if [ $2 ]; then
 	EMBY_URL=$(cat $2/emby_server.txt)
 	xiaoya_config_dir=$2
@@ -78,9 +72,6 @@ if [[ $(docker ps -a | grep -E "(^|\s)$EMBY_NAME(\s|$)") ]];then
 	#echo $mount_paths
 	#printf "%s\n" "${mount_paths[@]}" > $media_lib/config/mount_paths.txt
 	docker inspect $EMBY_NAME | grep Destination | grep -vE "/config|/media|/etc/nsswitch.conf" | awk -F\" '{print $4}' > $media_lib/config/mount_paths.txt
-#else
-#	echo "您的输入有误，没有找到名字为$EMBY_NAME的容器！程序退出！"
-#	exit 1
 fi
 curl -s "${EMBY_URL}/Users?api_key=e825ed6f7f8f44ffa0563cddaddce14d"  > /tmp/emby.response
 echo "$data Emby 关闭中 ...."
@@ -100,59 +91,59 @@ mv  $media_lib/config/data/library.db $media_lib/config/data/library.org.db
 [[ -f $media_lib/config/data/library.db-shm ]] && mv $media_lib/config/data/library.db-shm $media_lib/config/data/library.db-shm.bak
 #rm $media_lib/config/data/library.db*
 	
-# # 下载解压新config数据
-# if command -v ifconfig > /dev/null 2>&1; then
-	# docker0=$(ifconfig docker0 | awk '/inet / {print $2}' | sed 's/addr://')
-# else
-	# docker0=$(ip addr show docker0 | awk '/inet / {print $2}' | cut -d '/' -f 1)
-# fi
-# echo -e "测试xiaoya的联通性..."
-# if curl -siL http://127.0.0.1:5678/d/README.md | grep -v 302 | grep "x-oss-"; then
-	# xiaoya_addr="http://127.0.0.1:5678"
-# elif curl -siL http://${docker0}:5678/d/README.md | grep -v 302 | grep "x-oss-"; then
-	# xiaoya_addr="http://${docker0}:5678"
-# else
-	# if [ -s ${xiaoya_config_dir}/docker_address.txt ]; then
-		# docker_address=$(head -n1 ${xiaoya_config_dir}/docker_address.txt)
-		# if curl -siL http://${docker_address}:5678/d/README.md | grep -v 302 | grep "x-oss-"; then
-			# xiaoya_addr=${docker_address}
-		# else
-			# ERROR "请检查xiaoya是否正常运行后再试"
-			# exit 1
-		# fi
-	# else
-		# ERROR "请先配置 ${CONFIG_DIR}/docker_address.txt 后重试"
-		# exit 1
-	# fi
-# fi
-# echo -e "连接小雅地址为 ${xiaoya_addr}"
-# rm -f $media_lib/temp/config.mp4
-# docker run -i \
-	# --security-opt seccomp=unconfined \
-	# --rm \
-	# --net=host \
-	# -v ${media_lib}:/media \
-	# -v ${xiaoya_config_dir}:/etc/xiaoya \
-	# --workdir=/media/temp \
-	# -e LANG=C.UTF-8 \
-	# ailg/ggbond:latest \
-	# aria2c -o config.mp4 --continue=true -x6 --conditional-get=true --allow-overwrite=true "${xiaoya_addr}/d/元数据/config.mp4"
-# # 在temp下面解压，最终新config文件路径为temp/config
-# docker run -i \
-	# --security-opt seccomp=unconfined \
-	# --rm \
-	# --net=host \
-	# -v ${media_lib}:/media \
-	# -v ${xiaoya_config_dir}:/etc/xiaoya \
-	# --workdir=/media/temp \
-	# -e LANG=C.UTF-8 \
-	# ailg/ggbond:latest \
-	# 7z x -aoa -mmt=16 config.mp4
-# echo -e "下载解压元数据完成"
+# 下载解压新config数据
+if command -v ifconfig > /dev/null 2>&1; then
+	docker0=$(ifconfig docker0 | awk '/inet / {print $2}' | sed 's/addr://')
+else
+	docker0=$(ip addr show docker0 | awk '/inet / {print $2}' | cut -d '/' -f 1)
+fi
+echo -e "测试xiaoya的联通性..."
+if curl -siL http://127.0.0.1:5678/d/README.md | grep -v 302 | grep "x-oss-"; then
+	xiaoya_addr="http://127.0.0.1:5678"
+elif curl -siL http://${docker0}:5678/d/README.md | grep -v 302 | grep "x-oss-"; then
+	xiaoya_addr="http://${docker0}:5678"
+else
+	if [ -s ${xiaoya_config_dir}/docker_address.txt ]; then
+		docker_address=$(head -n1 ${xiaoya_config_dir}/docker_address.txt)
+		if curl -siL http://${docker_address}:5678/d/README.md | grep -v 302 | grep "x-oss-"; then
+			xiaoya_addr=${docker_address}
+		else
+			ERROR "请检查xiaoya是否正常运行后再试"
+			exit 1
+		fi
+	else
+		ERROR "请先配置 ${CONFIG_DIR}/docker_address.txt 后重试"
+		exit 1
+	fi
+fi
+echo -e "连接小雅地址为 ${xiaoya_addr}"
+rm -f $media_lib/temp/config.mp4
+docker run -i \
+	--security-opt seccomp=unconfined \
+	--rm \
+	--net=host \
+	-v ${media_lib}:/media \
+	-v ${xiaoya_config_dir}:/etc/xiaoya \
+	--workdir=/media/temp \
+	-e LANG=C.UTF-8 \
+	ailg/ggbond:latest \
+	aria2c -o config.mp4 --continue=true -x6 --conditional-get=true --allow-overwrite=true "${xiaoya_addr}/d/元数据/config.mp4"
+# 在temp下面解压，最终新config文件路径为temp/config
+docker run -i \
+	--security-opt seccomp=unconfined \
+	--rm \
+	--net=host \
+	-v ${media_lib}:/media \
+	-v ${xiaoya_config_dir}:/etc/xiaoya \
+	--workdir=/media/temp \
+	-e LANG=C.UTF-8 \
+	ailg/ggbond:latest \
+	7z x -aoa -mmt=16 config.mp4
+echo -e "下载解压元数据完成"
 
 echo "$data 检查同步数据库完整性..."
 sleep 4
-read -ep "check sql\&library!"
+#read -ep "check sql\&library!"
 if ${SQLITE_COMMAND_3} sqlite3 /emby/config/data/library.db ".tables" |grep Chapters3 > /dev/null ; then
 	
 	echo -e "\033[32m$data 同步数据库数据完整\033[0m"
@@ -184,9 +175,7 @@ else
 	exit
 fi
 
-#check_start
-echo -e "emby_version=$emby_version"
-read -ep "check emby_version"
+check_start
 
 if [[ ! $emby_version == 4.8.0.56 ]];then   
     docker stop ${EMBY_NAME}
@@ -196,9 +185,9 @@ if [[ ! $emby_version == 4.8.0.56 ]];then
     sleep 10
     check_start
 fi
-# rm -f $media_lib/config/*.sql
-# rm -f $media_lib/config/mount_paths.txt
-# rm -rf $media_lib/temp/config/*
+rm -f $media_lib/config/*.sql
+rm -f $media_lib/config/mount_paths.txt
+rm -rf $media_lib/temp/config/*
 
 EMBY_COMMAND="docker run -it --security-opt seccomp=unconfined --rm --net=host -v /tmp/emby.response:/tmp/emby.response -e LANG=C.UTF-8 ailg/ggbond:latest"
 USER_COUNT=$(${EMBY_COMMAND} jq '.[].Name' /tmp/emby.response |wc -l)
