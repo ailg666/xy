d6464d0955b97212a74b86f6f513a38d742f5870
ailg <ailg666@hotmail.com>
2024-03-07 23:55:16 +0800

    update new test files

--- sync_emby_config_new_ailg.sh
+++ sync_emby_config_new_ailg.sh
@@ -91,55 +91,55 @@ mv  $media_lib/config/data/library.db $media_lib/config/data/library.org.db
 [[ -f $media_lib/config/data/library.db-shm ]] && mv $media_lib/config/data/library.db-shm $media_lib/config/data/library.db-shm.bak
 #rm $media_lib/config/data/library.db*
 	
-# 下载解压新config数据
-if command -v ifconfig > /dev/null 2>&1; then
-	docker0=$(ifconfig docker0 | awk '/inet / {print $2}' | sed 's/addr://')
-else
-	docker0=$(ip addr show docker0 | awk '/inet / {print $2}' | cut -d '/' -f 1)
-fi
-echo -e "测试xiaoya的联通性..."
-if curl -siL http://127.0.0.1:5678/d/README.md | grep -v 302 | grep "x-oss-"; then
-	xiaoya_addr="http://127.0.0.1:5678"
-elif curl -siL http://${docker0}:5678/d/README.md | grep -v 302 | grep "x-oss-"; then
-	xiaoya_addr="http://${docker0}:5678"
-else
-	if [ -s ${xiaoya_config_dir}/docker_address.txt ]; then
-		docker_address=$(head -n1 ${xiaoya_config_dir}/docker_address.txt)
-		if curl -siL http://${docker_address}:5678/d/README.md | grep -v 302 | grep "x-oss-"; then
-			xiaoya_addr=${docker_address}
-		else
-			ERROR "请检查xiaoya是否正常运行后再试"
-			exit 1
-		fi
-	else
-		ERROR "请先配置 ${CONFIG_DIR}/docker_address.txt 后重试"
-		exit 1
-	fi
-fi
-echo -e "连接小雅地址为 ${xiaoya_addr}"
-rm -f $media_lib/temp/config.mp4
-docker run -i \
-	--security-opt seccomp=unconfined \
-	--rm \
-	--net=host \
-	-v ${media_lib}:/media \
-	-v ${xiaoya_config_dir}:/etc/xiaoya \
-	--workdir=/media/temp \
-	-e LANG=C.UTF-8 \
-	ailg/ggbond:test \
-	aria2c -o config.mp4 --continue=true -x6 --conditional-get=true --allow-overwrite=true "${xiaoya_addr}/d/元数据/config.mp4"
-# 在temp下面解压，最终新config文件路径为temp/config
-docker run -i \
-	--security-opt seccomp=unconfined \
-	--rm \
-	--net=host \
-	-v ${media_lib}:/media \
-	-v ${xiaoya_config_dir}:/etc/xiaoya \
-	--workdir=/media/temp \
-	-e LANG=C.UTF-8 \
-	ailg/ggbond:test \
-	7z x -aoa -mmt=16 config.mp4
-echo -e "下载解压元数据完成"
+# # 下载解压新config数据
+# if command -v ifconfig > /dev/null 2>&1; then
+	# docker0=$(ifconfig docker0 | awk '/inet / {print $2}' | sed 's/addr://')
+# else
+	# docker0=$(ip addr show docker0 | awk '/inet / {print $2}' | cut -d '/' -f 1)
+# fi
+# echo -e "测试xiaoya的联通性..."
+# if curl -siL http://127.0.0.1:5678/d/README.md | grep -v 302 | grep "x-oss-"; then
+	# xiaoya_addr="http://127.0.0.1:5678"
+# elif curl -siL http://${docker0}:5678/d/README.md | grep -v 302 | grep "x-oss-"; then
+	# xiaoya_addr="http://${docker0}:5678"
+# else
+	# if [ -s ${xiaoya_config_dir}/docker_address.txt ]; then
+		# docker_address=$(head -n1 ${xiaoya_config_dir}/docker_address.txt)
+		# if curl -siL http://${docker_address}:5678/d/README.md | grep -v 302 | grep "x-oss-"; then
+			# xiaoya_addr=${docker_address}
+		# else
+			# ERROR "请检查xiaoya是否正常运行后再试"
+			# exit 1
+		# fi
+	# else
+		# ERROR "请先配置 ${CONFIG_DIR}/docker_address.txt 后重试"
+		# exit 1
+	# fi
+# fi
+# echo -e "连接小雅地址为 ${xiaoya_addr}"
+# rm -f $media_lib/temp/config.mp4
+# docker run -i \
+	# --security-opt seccomp=unconfined \
+	# --rm \
+	# --net=host \
+	# -v ${media_lib}:/media \
+	# -v ${xiaoya_config_dir}:/etc/xiaoya \
+	# --workdir=/media/temp \
+	# -e LANG=C.UTF-8 \
+	# ailg/ggbond:test \
+	# aria2c -o config.mp4 --continue=true -x6 --conditional-get=true --allow-overwrite=true "${xiaoya_addr}/d/元数据/config.mp4"
+# # 在temp下面解压，最终新config文件路径为temp/config
+# docker run -i \
+	# --security-opt seccomp=unconfined \
+	# --rm \
+	# --net=host \
+	# -v ${media_lib}:/media \
+	# -v ${xiaoya_config_dir}:/etc/xiaoya \
+	# --workdir=/media/temp \
+	# -e LANG=C.UTF-8 \
+	# ailg/ggbond:test \
+	# 7z x -aoa -mmt=16 config.mp4
+# echo -e "下载解压元数据完成"
 
 echo "$data 检查同步数据库完整性..."
 sleep 4
