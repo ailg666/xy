e6e169496f52fc91acbea4bd57a129309a43751d
ailg <ailg666@hotmail.com>
2024-05-11 20:45:27 +0800

    update hlw.sh

--- hlw.sh
+++ hlw.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-docker run -d --name emby -v /etc/nsswitch.conf:/etc/nsswitch.conf -v /mnt/media_rw/d48ded09-158b-4536-b78b-0279c6936327/.ugreen_nas/312373/Docker/emby-xy/config:/config -v /mnt/media_rw/d48ded09-158b-4536-b78b-0279c6936327/.ugreen_nas/312373/Docker/emby-xy/xiaoya:/media -v /mnt/dm-0/.ugreen_nas/312373/影视:/movie1 -v /mnt/dm-2/.ugreen_nas/312373/影视:/movie2 --user 0:0 -p 9096:6908 -p 9920:8920 -p 1909:1900/udp -p 7399:7359/udp --device /dev/dri:/dev/dri --privileged --add-host="xiaoya.host:192.168.0.105" --restart always amilys/embyserver:4.8.0.56
+docker run -d --name emby -v /etc/nsswitch.conf:/etc/nsswitch.conf \
+-v /volume1/docker/embymeitiku/config:/config \
+-v /volume1/docker/embymeitiku/xiaoya:/media \
+-e LANG=C.UTF-8 \
+--user 0:0 \
+--net=host \
+--device /dev/dri:/dev/dri \
+--privileged --add-host="xiaoya.host:127.0.0.1" --restart always amilys/embyserver:4.8.0.56
