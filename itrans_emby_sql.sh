2294e5fd772fc8e3e8b54f9c9633dd763381ba37
ailg <ailg666@hotmail.com>
2024-05-30 10:45:44 +0800

    update itrans_emby_sql.sh

--- itrans_emby_sql.sh
+++ itrans_emby_sql.sh
@@ -7,21 +7,54 @@ exec 3>"${target_sql}"
 
 while IFS= read -r line
 do
-    # 检查行是否包含特定的表名
     if [[ $line == *"AncestorIds2"* ]]; then
-        echo $line | awk -F'[(),]' '{if ($3 < 10) {$2=$2+1000000} else {$2=$2+1000000;$3=$3+1000000};printf "%s(%d,%d,%d);\n", $1, $2, $3, $4}' >&3
+        echo $line | awk -F'VALUES' '{match($2, /\((.*)\)/, a); split(a[1],b,","); if (b[2] < 10) {b[1]=b[1]+1000000} else {b[1]=b[1]+1000000; b[2]=b[2]+1000000}; printf "%sVALUES(%d,%d,%d);\n", $1, b[1], b[2], b[3]}' >&3
     elif [[ $line == *"ItemLinks2"* ]]; then
-        echo $line | awk -F'[(,)]' '{if ($4 >= $2+1) {$4=$4+1000000}; $2=$2+1000000; printf "%s(%d,%d,%d,%d);\n", $1, $2, $3, $4, $5}' >&3
+        echo $line | awk -F'VALUES' '{match($2, /\((.*)\)/, a); split(a[1],b,","); if (b[3] >= b[1]+1) {b[3]=b[3]+1000000}; b[1]=b[1]+1000000; printf "%sVALUES(%d,%d,%d,%d);\n", $1, b[1], b[2], b[3], b[4]}' >&3
     elif [[ $line == *"MediaStreams2"* ]]; then
-        echo $line | awk -F'[(,)]' '{$2=$2+1000000; printf "%s(%d,%d,%s", $1, $2, $3, $4; for(i=5;i<NF;i++) printf ",%s", $i; printf ")%s\n",$NF}' >&3
+        echo $line | awk -F'VALUES' '{
+            match($2, /\((.*)\)/, a);
+            split(a[1],b,",");
+            if (b[1] != "NULL") b[1]=b[1]+1000000;
+            fmt = ("%d,%d,%d,%s,%s,%s,%s,%s,%s,%d,%d,%d,%d,%d,%d,%d,%d,%d,%f,%f,%f,%s,%d,%d,%d,%d,%s,%s,%s,%s,%s,%s,%s,%s,%s,%d,%s,%d,%d,%d");
+            split(fmt, f, ",");
+            printf "%sVALUES(", $1;
+            for(i=1;i<length(b);i++) {
+                if (b[i] != "NULL" && f[i] ~ /%d/) {
+                    printf f[i] ",", b[i];
+                } else {
+                    printf "%s,", b[i];
+                }
+            }
+            printf f[length(b)] ");\n", b[length(b)]
+        }' >&3
     elif [[ $line == *"MediaItems"* ]]; then
-        echo $line | awk -F'[(),]' '{if ($5 < 10) {$2=$2+1000000} else {$2=$2+1000000;$5=$5+1000000};printf "%s(%d,%s,%d,%d",$1, $2, $3, $4,$5;for(i=6;i<NF;i++) printf ",%s", $i; printf ")%s\n",$NF}' >&3
+        echo $line | awk -F'VALUES' '{
+            match($2, /\((.*)\)/, a);
+            split(a[1],b,",");
+            if (b[4] > 10) b[4]=b[4]+1000000;
+            b[1]=b[1]+1000000;
+            fmt = ("%d,%s,%d,%d,%s,%d,%d,%d,%d,%d,%d,%f,%s,%d,%d,%s,%s,%s,%d,%d,%d,%s,%d,%d,%d,%d,%d,%s,%s,%d,%d,%d,%s,%d,%d,%f,%s,%s,%d,%s,%s,%d,%d,%s,%s,%s,%s,%d,%d,%s,%s,%d,%d,%d,%d,%s,%d,%d,%d,%d,%d,%d,%d,%s,%s,%d");
+            split(fmt, f, ",");
+            printf "%sVALUES(", $1;
+            for(i=1;i<length(b);i++) {
+                if (b[i] != "NULL" && f[i] ~ /%d/) {
+                    printf f[i] ",", b[i];
+                } else {
+                    printf "%s,", b[i];
+                }
+            }
+            if (b[length(b)] != "NULL" && f[length(b)] ~ /%d/) {
+                printf f[length(b)] ");\n", b[length(b)];
+            } else {
+                printf "%s);\n", b[length(b)];
+            }
+        }' >&3
     elif [[ $line == *"ItemPeople2"* ]]; then
-        echo $line | awk -F'[(),]' '{$2=$2+1000000; printf"%s(%d,%d,%s,%d,%d);\n", $1, $2, $3, $4,$5,$6}' >&3
+        echo $line | awk -F'VALUES' '{match($2, /\((.*)\)/, a); split(a[1],b,","); b[1]=b[1]+1000000; printf "%sVALUES(%d,%d,%s,%d,%d);\n", $1,b[1],b[2],b[3],b[4],b[5]}' >&3
     else
         echo $line >&3
     fi
 done < "${source_sql}"
 
 exec 3>&-
-
