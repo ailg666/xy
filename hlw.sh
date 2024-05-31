7cd774af123ca16946a14adf0dd46861a65610a1
ailg <ailg666@hotmail.com>
2024-05-31 18:39:15 +0800

    Initial commit

--- hlw.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker run -d --name emby -v /etc/nsswitch.conf:/etc/nsswitch.conf \
--v /volume1/docker/embymeitiku/config:/config \
--v /volume1/docker/embymeitiku/xiaoya:/media \
--e LANG=C.UTF-8 \
---user 0:0 \
---net=host \
---device /dev/dri:/dev/dri \
---privileged --add-host="xiaoya.host:127.0.0.1" --restart always amilys/embyserver:4.8.0.56
--- hlw3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-USER_URL="http://192.168.0.105/Users?api_key=e825ed6f7f8f44ffa0563cddaddce14d"  
-response=$(curl -s "$USER_URL")
-read -r name <<< "$(echo "$response" | jq -r ".[0].Name")"  
-read -r id <<< "$(echo "$response" | jq -r ".[0].Id")"
-read -r policy <<< "$(echo "$response" | jq -r ".[0].Policy | to_entries | from_entries | tojson")"
-USER_URL_2="${EMBY_URL}/Users/$id/Policy?api_key=e825ed6f7f8f44ffa0563cddaddce14d"
-curl -i -H "Content-Type: application/json" -X POST -d "$policy" "$USER_URL_2"
